d18bf0fc0ef1ba16a9db61edb5c515ce4d7c9821
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-08-19 16:13:38 +0900

    work

---
+++ keras2/keras65_lambda.py
@@ -0,0 +1,10 @@
+gradient = lambda x: 2*x - 4
+
+def gradient2(x):
+    temp = 2*x -4
+    return temp
+
+x = 3 
+
+print(gradient(3))
+print(gradient2(3))
---
+++ keras2/keras66_gradient2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = lambda x : x**2 - 4*x + 6
+
+gradient = lambda x: 2*x -4 
+
+x0 = 10.0 # 초기값
+MaxIter = 20
+learning_rate = 0.3
+
+print("step\tx\tf(x)")
+print("{:02d}\t{:6.5f}\t{:6.5f}".format(0, x0, f(x0)))
+
+for i in range(MaxIter):
+    x1 = x0 - learning_rate * gradient(x0)
+    x0 = x1
+
+    print("{:02d}\t{:6.5f}\t{:6.5f}".format(i+1, x0, f(x0)))
+
+
+# 초기값 변경
+# 러닝레이트 변경 
